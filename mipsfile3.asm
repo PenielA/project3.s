62af29bbff2cd4479bf85b93a39377790b38c9ba
PenielA <36523791+PenielA@users.noreply.github.com>
2018-12-14 22:39:29 +0300

    This was my code for project 2 and I am going to modify it to process the user input in a subprogram and use the stack for parameters and return values.

---
+++ mipsfile3.asm
@@ -0,0 +1,174 @@
+.data 
+   null_errorMessage:   .asciiz  "Input is empty."
+   length_errorMessage:   .asciiz  "Input is too long."
+   base_errorMessage:   .asciiz   "Invalid base-31 number."
+   user_Input:   .space 1000
+   .text
+main:
+   li $v0, 8    
+   la $a0, user_Input  
+   li $a1, 1000
+   syscall 
+removespaceinfront:
+   li $t8, 32                              
+   lb $t7, 0($a0)   
+   beq $t8, $t7, removefirstcharacter     
+   move $t7, $a0                          
+   j removespaceafter
+removefirstcharacter:
+   addi $a0, $a0, 1
+   j removespaceinfront
+removespaceafter:
+   la $t2, user_Input
+   sub $t2, $t7, $t2 
+   li $t1, 0  
+   li $t8, 0  
+removespaceafter_loop:
+   add $t4, $t2, $t8
+   addi $t4, $t4, -100
+   beqz $t4, end_removespaceafter        
+   add $t4, $t8, $a0
+   lb $t4, 0($t4)          
+   beq $t4, $zero, end_removespaceafter  
+   addi $t4, $t4, -10
+   beqz $t4, end_removespaceafter        
+   addi $t4, $t4, -22
+   bnez $t4, updatelastIndex 
+removespaceafter_increment:
+   addi $t8, $t8, 1              
+   j removespaceafter_loop
+updatelastIndex:
+   move $t1, $t8 
+   j removespaceafter_increment
+end_removespaceafter:
+   add $t4, $zero, $a0 
+   add $t4, $t4, $t1 
+   addi $t4, $t4, 1  
+   j checkLength
+checkLength:
+   li $t0, -1
+   add $a0, $t7, $zero
+checkLength_Loop:
+   lb $t2, 0($a0)
+   or $t1, $t2, $t0
+   beq $t1, $zero, null_error
+   beq $t2, $zero, stringDone
+   addi $a0, $a0, 1
+   addi $t0, $t0, 1
+   j checkLength_Loop
+stringDone:
+   slti $t5, $t0, 5
+   beq $t5, $zero, length_error
+   bne $t5, $zero, check_String
+null_error:
+   li $v0, 4
+   la $a0, null_errorMessage
+   syscall
+   j end
+length_error:
+   li $v0, 4
+   la $a0, length_errorMessage
+   syscall
+   j end
+check_String:
+   move $a0, $t7 
+check_StringLoop:
+   li $v0, 11
+   lb $t3, 0($a0)
+   move $t8, $a0
+   move $a0, $t3
+   move $a0, $t8
+   li $t8, 10              
+   beq $t3, $t8, base_converter
+   slti $t4, $t3, 48        
+   bne $t4, $zero, base_error
+   slti $t4, $t3, 58       
+   bne $t4, $zero, Increment
+   slti $t4, $t3, 65        
+   bne $t4, $zero, base_error  
+   slti $t4, $t3, 86    
+   bne $t4, $zero, Increment
+   slti $t4, $t3, 97      
+   bne $t4, $zero, base_error 
+   slti $t4, $t3, 121      
+   bne $t4, $zero, Increment
+   bgt $t3, 117, base_error 
+   li $t8, 10              
+   beq $t3, $t8, base_converter     
+Increment:
+   addi $a0, $a0, 1
+   j check_StringLoop
+base_error:
+   li $v0, 4
+   la $a0, base_errorMessage
+   syscall
+   j end
+base_converter:
+   move $a0, $t7  
+   li $t1, 10
+   li $t8, 0  
+   add $s7, $s7, $t0
+   addi $s7, $s7, -1 
+   li $s2, 3
+   li $s3, 2
+   li $s6, 1
+   li $s1, 0
+convertString:     
+   lb $s5, 0($a0)
+   beqz $s5, Totalsum		
+   beq $s5, $t2, Totalsum    	
+   slti $t4, $s5, 58        
+   bne $t4, $zero, zero_to_nine
+   slti $t4, $s5, 86     
+   bne $t4, $zero, A_to_U
+   slti $t4, $s5, 118   
+   bne $t4, $zero, a_to_u
+zero_to_nine:
+   addi $s5, $s5, -48
+   j next_step
+A_to_U:
+   addi $s5, $s5, -55
+   j next_step
+a_to_u:
+   addi $s5, $s5, -87
+next_step:
+   beq $s7, $s2, Base_raised_toThree
+   beq $s7, $s3, Base_raised_toTwo
+   beq $s7, $s6, Base_raised_toOne
+   beq $s7, $s1, Base_raised_toZero
+Base_raised_toThree:
+   li $s4, 29791
+   mult $s5, $s4
+   mflo $s0
+   add $t8, $t8, $s0
+   addi $s7, $s7, -1
+   addi $a0, $a0, 1
+   j convertString
+Base_raised_toTwo:
+   li $s4, 961
+   mult $s5, $s4
+   mflo $s0
+   add $t8, $t8, $s0
+   addi $s7, $s7, -1
+   addi $a0, $a0, 1
+   j convertString
+Base_raised_toOne:
+   li $s4, 31
+   mult $s5, $s4
+   mflo $s0
+   add $t8, $t8, $s0
+   addi $s7, $s7, -1
+   addi $a0, $a0, 1
+   j convertString
+Base_raised_toZero:
+   li $s4, 1
+   mult $s5, $s4
+   mflo $s7
+   add $t8, $t8, $s7
+Totalsum:
+   li $v0, 1
+   move $a0, $t8
+   syscall
+end:
+   li $v0,10      
+   syscall       
